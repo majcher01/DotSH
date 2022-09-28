f1a16346d2906eec6a797b879445e140c2546fa0
Jakub Majcherski <majcherski.jakub@gmail.com>
2022-09-28 12:00:53 +0200

    server type

--- vhost-rproxy/vhost.sh
+++ vhost-rproxy/vhost.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# usage with parameters vhost.sh <filename> <domain> <port>
+# usage with parameters vhost.sh <filename> <domain> <port> <server-type>
 
-if [ $# -eq 3 ]
+if [ $# -gt 2 ]
 then
+
+	if [[ $4 = "httpd" ]]
+	then
+		cp template-redhat.conf $1
+		sed -i "s/DOMENA/$2/" $1
+		sed -i "s/PORT/$3/" $1
+		exit 0
+	fi
 	
 	cp template.conf $1
 	sed -i "s/DOMENA/$2/" $1
@@ -11,13 +19,23 @@ then
 	exit 0
 fi
 
-read -p "podaj pelna nazwe pliku "
+read -p "server type (apache/httpd) "
+srvtp=$REPLY
+
+if [[ $srvtp = "httpd" ]]
+then
+	templatefile="template-redhat.conf"
+else
+	templatefile="template.conf"
+fi
+
+read -p "full filename "
 nazwapliku=$REPLY
-cp template.conf $nazwapliku
-read -p "podaj domene "
+cp $templatefile $nazwapliku
+read -p "domain "
 domena=$REPLY
 sed -i "s/DOMENA/$domena/" $nazwapliku
-read -p "podaj port  "
+read -p "port "
 port=$REPLY
 sed -i "s/PORT/$port/" $nazwapliku
 exit 0
